0cdea09d35d3710674f3508acbbd8485962bbe8b
Raven Devore <ravendevore@my.unt.edu>
2022-10-12 21:02:33 -0500

    Added PDF viewer

--- WactzioFE/src/views/Home.vue
+++ WactzioFE/src/views/Home.vue
@@ -2,5 +2,10 @@
     <main class="home-page">
         <h1>Wactz.io Home</h1>
         <p>This is the home for the Wactz.io front end.</p>
+        <pdfviewer />
     </main>
 </template>
+
+<script setup>
+import pdfviewer from '../components/PDFViewer.vue'
+</script>
