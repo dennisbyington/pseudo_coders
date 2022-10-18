b9b2e5c62d5508eaccd8ee28671d7afb2057519c
Dennis Byington <dennisbyington@mac.com>
2022-10-17 20:18:43 -0500

    Added Documents Page.  Installed Axios.  Linked to backend via CORS.

---
+++ WactzioFE/src/views/Documents.vue
@@ -0,0 +1,34 @@
+<!-- <template>
+    <main class="documents-page">
+        <h1>Documents</h1>
+        <p>This is the documents page</p>
+    </main>
+</template> -->
+
+<template>
+  <div id="app">
+    <Documents />
+  </div>
+</template>
+
+<script>
+import Documents from '../components/Documents.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Documents
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
