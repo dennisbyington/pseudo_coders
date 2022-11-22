d62c31e6d47a769e982841540988910bc66b8690
Raven Devore <ravendevore@my.unt.edu>
2022-11-21 20:31:11 -0600

    Toggle bounding boxes button now acts on all pages

--- WactzioFE/src/components/PDFViewer.vue
+++ WactzioFE/src/components/PDFViewer.vue
@@ -44,7 +44,28 @@
     [
       [50,50,350,100],   // flow 0
       [375,375,400,400], // flow 1
-    ]  // page 2
+    ], // page 2
+    [
+
+    ], // page 3
+    [
+      [10,10,20,20],
+      [30,30,40,40],
+      [50,50,60,60],
+      [70,70,80,80],
+      [90,90,100,100],
+      [110,110,120,120],
+      [130,130,140,140],
+      [150,150,160,160],
+      [170,170,180,180],
+      [190,190,200,200],
+      [210,210,220,220],
+      [230,230,240,240],
+      [250,250,260,260],
+      [270,270,280,280],
+      [290,290,300,300],
+      [310,310,320,320],
+    ]  // page 4
   ]
 
 </script>
@@ -72,15 +93,16 @@ export default {
     },
   },
   methods: {
-    async toggleBboxesView() { // for current page only (FIXME?)
+    async toggleBboxesView() { // for all pages
       if (this.currInstance) { // make sure instance exists before modifying it
-        let currentPage = this.currInstance.viewState.currentPageIndex
-        const annotations = await this.currInstance.getAnnotations(currentPage)
-        annotations.forEach((annotation) => {
-          if (annotation.subject === "bounding-box") {
-            this.currInstance.update(annotation.set("noView", !annotation.noView).set("noPrint", !annotation.noPrint))
-          }
-        })
+        for (let page = 0; page < this.currInstance.totalPageCount; page++) {
+          const annotations = await this.currInstance.getAnnotations(page)
+          annotations.forEach((annotation) => {
+            if (annotation.subject === "bounding-box") {
+              this.currInstance.update(annotation.set("noView", !annotation.noView).set("noPrint", !annotation.noPrint))
+            }
+          })
+        }
       }
     },
     prevDoc() {
