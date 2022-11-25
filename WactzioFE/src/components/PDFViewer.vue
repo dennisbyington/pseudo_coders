d98ca26988d6d693990b792d5f87fbde416efdf3
Raven Devore <ravendevore@my.unt.edu>
2022-11-25 17:41:58 -0600

    Bounding boxes now load from backend

--- WactzioFE/src/components/PDFViewer.vue
+++ WactzioFE/src/components/PDFViewer.vue
@@ -26,44 +26,43 @@
   import { useFileMgrStore } from "../stores/fileMgr"
   const store = useFileMgrStore()
   const currentFileWatch = computed(() => store.currentFile ) // can't directly watch a store variable, so use computed
+  let bboxes = []
 
-  let bboxes = // not accurate, just to make sure loading works
-  [
-    [
-      [100,100,200,200], // flow 0
-      [250,250,350,350], // flow 1
-      [400,400,500,500]  // flow 2
-    ], // page 0
-    [
-      [50,50,150,150], // flow 0
-    ], // page 1
-    [
-      [50,50,350,100],   // flow 0
-      [375,375,400,400], // flow 1
-    ], // page 2
-    [
-
-    ], // page 3
-    [
-      [10,10,20,20],
-      [30,30,40,40],
-      [50,50,60,60],
-      [70,70,80,80],
-      [90,90,100,100],
-      [110,110,120,120],
-      [130,130,140,140],
-      [150,150,160,160],
-      [170,170,180,180],
-      [190,190,200,200],
-      [210,210,220,220],
-      [230,230,240,240],
-      [250,250,260,260],
-      [270,270,280,280],
-      [290,290,300,300],
-      [310,310,320,320],
-    ]  // page 4
-  ]
-
+  const bboxIconVisible = `<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" fill="cornflowerblue" stroke="cornflowerblue" stroke-width="2"><rect width="20" height="20" /></svg>`
+  const bboxIconHidden = `<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" fill="none" stroke="cornflowerblue" stroke-width="2"><rect width="20" height="20" /></svg>`
+  let toggleBboxesItem = { // toolbar button to toggle bounding box visibility
+    type: "custom",
+    id: "toggle-bboxes",
+    title: "Toggle Bounding Boxes",
+    icon: bboxIconVisible,
+    buttonInstance: null,
+    setInstance(newInstance) {
+      this.buttonInstance = newInstance
+    },
+    async onPress() {
+      let tempInstance = toggleBboxesItem.buttonInstance
+      if (tempInstance) { // make sure instance exists before modifying it
+        // show/hide the annotations as needed
+        for (let page = 0; page < tempInstance.totalPageCount; page++) {
+          const annotations = await tempInstance.getAnnotations(page)
+          annotations.forEach((annotation) => {
+            if (annotation.subject === "bounding-box") {
+              tempInstance.update(annotation.set("noView", !annotation.noView).set("noPrint", !annotation.noPrint))
+            }
+          })
+        }
+        // change the button's appearance to match the current visibility of the annotations
+        tempInstance.setToolbarItems(toolbarItems =>
+          toolbarItems.map(tempItem => {
+            if (tempItem.id === "toggle-bboxes") {
+              tempItem.icon = (tempItem.icon === bboxIconVisible) ? bboxIconHidden : bboxIconVisible
+            }
+            return tempItem;
+          })
+        )
+      }
+    }
+  } // end of toolbar button
 </script>
 
 <script>
@@ -80,92 +79,79 @@ export default {
      * */ 
     this.store.currentFile = ""
     this.store.currentIndex = -1
+
+    // Setting these to default just in case (FIXME?)
+    this.store.backendCompanyID = -1
+    this.store.backendDocID = -1
   },
   watch: {
     currentFileWatch(val) { // used for loading documents accessed through home page
       if (val) {
-        this.loadDoc()
+        PSPDFKit.unload(".pdf-container") // Hide old PDF
+        this.axiosBboxThenLoad(3) // try to get bboxes from backend then load document, 3 attempts max
       }
     },
   },
   methods: {
+    axiosBboxThenLoad(max_retries) {
+      if (max_retries <= 0) { // No more attempts, display error
+        alert("Error: Unable to load bounding boxes for this document.")
+      }
+      else {
+        this.$axios.get("http://localhost:3000/api/v0/companies/bbox/" + this.store.backendCompanyID + "/" + this.store.backendDocID).then( bboxResult => {
+            this.bboxes = bboxResult.data // Store the bbox data
+            if (this.bboxes.length > 0) { // Successfully got bbox data
+              this.loadDoc()              // Load the PDF
+            }
+            else { // Failure, try loading again
+              this.axiosBbox(max_retries - 1)
+            }
+        })
+      }
+    },
     prevDoc() {
       this.store.currentIndex--
     },
     nextDoc() {
       this.store.currentIndex++
     },
-    loadDoc() { // Will load PDF and its annotations (currenly only loads PDF)
-
-      
-      let toggleInstance = null
-      const bboxIconVisible = `<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" fill="cornflowerblue" stroke="cornflowerblue" stroke-width="2"><rect width="20" height="20" /></svg>`
-      const bboxIconHidden = `<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" fill="none" stroke="cornflowerblue" stroke-width="2"><rect width="20" height="20" /></svg>`
-
-      const toggleBboxesItem = { // toolbar button to toggle bounding box visibility
-        type: "custom",
-        id: "toggle-bboxes",
-        title: "Toggle Bounding Boxes",
-        icon: bboxIconVisible,
-        async onPress() {
-          if (toggleInstance) { // make sure instance exists before modifying it
-            // show/hide the annotations as needed
-            for (let page = 0; page < toggleInstance.totalPageCount; page++) {
-              const annotations = await toggleInstance.getAnnotations(page)
-              annotations.forEach((annotation) => {
-                if (annotation.subject === "bounding-box") {
-                  toggleInstance.update(annotation.set("noView", !annotation.noView).set("noPrint", !annotation.noPrint))
-                }
-              })
-            }
-            // change the button's appearance to match the current visibility of the annotations
-            toggleInstance.setToolbarItems(toolbarItems =>
-              toolbarItems.map(tempItem => {
-                if (tempItem.id === "toggle-bboxes") {
-                  tempItem.icon = (tempItem.icon === bboxIconVisible) ? bboxIconHidden : bboxIconVisible
-                }
-                return tempItem;
-              })
-            )
-          }
-        }
-      } // end of toolbar button
-
-      console.log("PDFViewer.vue: Loading PDF with name",this.store.currentFile)
-      PSPDFKit.unload(".pdf-container")
+    createAnnotations(storedInstance) {
+      for (let page = 0; page < this.bboxes.length; page++) { // iterate through pages
+        for (let flow = 0; flow < this.bboxes[page].length; flow++) { // iterate through flows
+          // this.bboxes[page][flow] = [xMin, yMin, xMax, yMax]
+          const annotation = new PSPDFKit.Annotations.RectangleAnnotation({ // rectangle annotation definition
+              pageIndex: page,
+              boundingBox: new PSPDFKit.Geometry.Rect({
+                left: this.bboxes[page][flow][0],                                // xMin
+                top: this.bboxes[page][flow][1],                                 // yMin
+                width: this.bboxes[page][flow][2] - this.bboxes[page][flow][0],  // xMax - xMin
+                height: this.bboxes[page][flow][3] - this.bboxes[page][flow][1], // yMax - yMin
+              }),
+              fillColor: PSPDFKit.Color.BLUE,
+              opacity: 0.5,
+              isEditable: false,
+              subject: "bounding-box"
+          }) // end of rectangle annotation definition
+          storedInstance.create(annotation) // actually create the annotation
+        } // end of iterating through flows
+      } // end of iterating through pages
+    },
+    loadDoc() { // Will load PDF and create annotations
+      let storedInstance = null
       PSPDFKit.load({
         document: this.store.currentFile,
         container: ".pdf-container",
         disableWebAssemblyStreaming: true,
-        toolbarItems: [...PSPDFKit.defaultToolbarItems, toggleBboxesItem],
+        toolbarItems: [...PSPDFKit.defaultToolbarItems, this.toggleBboxesItem],
         isEditableAnnotation: function(annotation) { // prevents editing only the bounding box annotations
           return annotation.subject !== "bounding-box";
         },
-      }).then(async (instance) => { // start of annotation loading   
-        toggleInstance = instance // need to store for use in toggling bounding box visibility
-        // create annotations
-        for (let page = 0; page < this.bboxes.length; page++) { // iterate through pages
-          for (let flow = 0; flow < this.bboxes[page].length; flow++) { // iterate through flows
-            // this.bboxes[page][flow] = [xMin, yMin, xMax, yMax]
-            const annotation = new PSPDFKit.Annotations.RectangleAnnotation({ // rectangle annotation definition
-                pageIndex: page,
-                boundingBox: new PSPDFKit.Geometry.Rect({
-                  left: this.bboxes[page][flow][0],                                // xMin
-                  top: this.bboxes[page][flow][1],                                 // yMin
-                  width: this.bboxes[page][flow][2] - this.bboxes[page][flow][0],  // xMax - xMin
-                  height: this.bboxes[page][flow][3] - this.bboxes[page][flow][1], // yMax - yMin
-                }),
-                fillColor: PSPDFKit.Color.BLUE,
-                opacity: 0.5,
-                isEditable: false,
-                subject: "bounding-box"
-            }) // end of rectangle annotation definition
-            instance.create(annotation) // actually create the annotation
-          } // end of iterating through flows
-        } // end of iterating through pages
-      }) // end of annotation loading
-
-    }, // end of loadDoc()
+      }).then(async (instance) => { // annotation loading   
+        storedInstance = instance                         // need to store for use in toggling bounding box visibility, loading annotations
+        this.toggleBboxesItem.setInstance(storedInstance) // needed for toggle to work properly
+        this.createAnnotations(storedInstance)            // create the annotations
+      })
+    },
     getFileName() {
       if (this.store.currentFile) {
         let encodedName = this.store.currentFile.substring(this.store.currentFile.lastIndexOf("/")+1,) // Gets a substring of everything after the last "/"
--- WactzioFE/src/stores/fileMgr.js
+++ WactzioFE/src/stores/fileMgr.js
@@ -2,6 +2,6 @@ import { defineStore } from "pinia"
 
 export const useFileMgrStore = defineStore("fileMgr", {
     state: () => {
-        return { currentFile: "", currentIndex: -1, arrayLength: 0 }
+        return { currentFile: "", backendCompanyID: -1, backendDocID: -1, currentIndex: -1, arrayLength: 0 }
     },
 });
